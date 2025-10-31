ea0908190aac654f8a7bda5f1a32d21873fa6d28
weichaozeng <zvc22@outlook.com>
2025-10-31 11:23:35 +0800

    Update infer

--- infer.py
+++ infer.py
@@ -11,7 +11,8 @@ from openpose import OpenposeDetector
 
 if __name__ == '__main__' :
     
-    image_dirs = ["./assets/test.jpeg", "./assets/test1.jpeg", "./assets/test2.jpeg", "./assets/test3.jpeg"]
+    # image_dirs = ["./assets/test.jpeg", "./assets/test1.jpeg", "./assets/test2.jpeg", "./assets/test3.jpeg"]
+    image_dirs = ["/home/zvc/Data/VHand/DWPose/inputs/demo_0.jpg"]
     
     det_config = './dwpose/yolox_config/yolox_l_8xb8-300e_coco.py'
     det_ckpt = './ckpts/yolox_l_8x8_300e_coco_20211126_140236-d3bd2b23.pth'
@@ -24,7 +25,8 @@ if __name__ == '__main__' :
     annotator_ckpts_path = "./ckpts"
     preprocessor = OpenposeDetector(annotator_ckpts_path)
 
-    save_dir = "./outputs"
+    # save_dir = "./outputs"
+    save_dir = "/home/zvc/Data/VHand/DWPose/outputs"
     os.makedirs(save_dir, exist_ok=True)
 
     for image_dir in image_dirs:
